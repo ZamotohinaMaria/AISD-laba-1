a88b151d27bd0affc644ea4112e1fcd726647edf
ZamotohinaMaria <zamotokhina.masha@mail.ru>
2022-09-24 18:39:52 +0400

    добавлены определения методов класса, перегрузки некоторых операторов и написан примерный код основной прораммы

---
+++ Vector.cpp
@@ -0,0 +1,198 @@
+#include <iostream>
+#include <stdio.h>
+#include "Vectors.h"
+
+using namespace std;
+
+double Vectors:: MIN_D = 0.00000001;
+
+Vectors::Vectors(unsigned size) : size(size)
+{
+	vector = new double[size];
+	for (int i = 0; i < size; i++)
+	{
+		vector[i] = 0;
+	}
+}
+
+Vectors::Vectors(const Vectors& v)
+{
+	size = v.size;
+	vector = new double[size];
+	for (int i = 0; i < size; i++)
+	{
+		vector[i] = v.vector[i];
+	}
+}
+
+Vectors::~Vectors()
+{
+	delete[] vector;
+}
+
+unsigned Vectors:: GetSize() const
+{
+	return size;
+}
+
+void Vectors::SetVector()
+{
+	cout << "Input vector size" << endl;
+	int x;
+	cin >> x;
+	size = x;
+	cout << "Input vector values" << endl;
+	for (int i = 0; i < size; i++)
+	{
+		cin >> x;
+		vector[i] = x;
+	}
+}
+
+void Vectors:: PrintVector() const
+{
+	cout << "Vector parameters:" << endl;
+	cout << "size = " << size << endl;
+	cout << "Values:";
+	for (int i = 0; i < size; i++)
+	{
+		cout << vector[i] << " ";
+	}
+	cout << endl << endl;
+}
+
+Vectors& Vectors:: operator = (const Vectors& v)
+{
+	if (this == (&v))
+	{
+		return*this;
+	}
+	if (size < v.size)
+	{
+		delete[] vector;
+		vector = NULL;
+		vector = new double[v.size];
+	}
+	for (int i = 0; i < v.size; i++)
+	{
+		vector[i] = v.vector[i];
+	}
+	size = v.size;
+	return *this;
+}
+
+double& Vectors:: operator [] (const unsigned& i) const
+{
+	if (i >= size || i < 0)
+	{
+		throw "index i is out of range";
+	}
+	for (int j = 0; j < size; j++)
+	{
+		if (i == j)
+		{
+			return vector[i];
+		}
+	}
+}
+
+Vectors Vectors:: operator + (const Vectors& v)
+{
+	if (size != v.size)
+	{
+		throw "sizes of vectors are different";
+	}
+	Vectors res(size);
+	for (int i = 0; i < size; i++)
+	{
+		res.vector[i] = vector[i] + v.vector[i];
+	}
+	return res;
+}
+
+Vectors Vectors:: operator - (const Vectors& v)
+{
+	if (size != v.size)
+	{
+		throw "sizes of vectors are different";
+	}
+	Vectors res(size);
+	for (int i = 0; i < size; i++)
+	{
+		res.vector[i] = vector[i] - v.vector[i];
+	}
+	return res;
+}
+
+double Vectors:: operator * (const Vectors& v)
+{
+	if (size != v.size)
+	{
+		throw "sizes of vectors are different";
+	}
+	double res = 0;
+	for (int i = 0; i < size; i++)
+	{
+		res += vector[i] * v.vector[i];
+	}
+	return res;
+}
+
+Vectors Vectors:: operator * (const int& c)
+{
+	Vectors res(size);
+	for (int i = 0; i < size; i++)
+	{
+		res.vector[i] = vector[i] * c;
+	}
+	return res;
+}
+
+Vectors Vectors:: operator / (const int& c)
+{
+	if (c == 0)
+	{
+		throw "c can not be a 0";
+	}
+	Vectors res(size);
+	for (int i = 0; i < size; i++)
+	{
+		res.vector[i] = vector[i] / c;
+	}
+	return res;
+}
+
+bool Vectors:: operator == (const Vectors& v) const
+{
+	if (size == v.size)
+	{
+		for (int i = 0; i < size; i++)
+		{
+			if (abs(v.vector[i] - vector[i]) < MIN_D)
+			{
+				return 1;
+			}
+		}
+	}
+	return 0;
+}
+
+bool Vectors:: operator != (const Vectors& v) const
+{
+	if (size != v.size)
+	{
+		return 0;
+	}
+	else
+	{
+		for (int i = 0; i < size; i++)
+		{
+			if (abs(v.vector[i] - vector[i]) > MIN_D)
+			{
+				return 0;
+			}
+		}
+	}
+		
+	return 1;
+}
--- main.cpp
+++ main.cpp
@@ -1,8 +1,53 @@
+// variant 1(2)
 #include <iostream>
+#include <cmath>
+#include "Vectors.h"
+
 using namespace std;
+
+//std::ostream& operator<< (std::ostream& out, const Vectors& point)
+//{
+//	out << "Point(" << point.m_x << ", " << point.m_y << ", " << point.m_z << ')';
+//	return out;
+//}
+
+std::ostream& operator<< (std::ostream& out, const Vectors& v)
+{
+	out << "Vector:";
+	for (int i = 0; i < v.size; i++)
+	{
+		out << v.vector[i];
+	}
+	return out;
+}
+
+Vectors operator * (const int& c, const Vectors& v)
+{
+	Vectors res(v.GetSize());
+	for (int i = 0; i < v.GetSize(); i++)
+	{
+		res.operator[](i) = v.operator[](i) * c;
+	}
+	return res;
+}
 
 int main()
 {
-	cout << hello, world << endl;
+	Vectors a, b, c;
+	a.SetVector();
+	b.SetVector();
+	try
+	{
+		// c=1/2(a+b)
+		c = a.operator+(b);
+		//c = (a.operator+(b)).operator*(0.5);
+		//c.operator=((a.operator+(b)).operator*(0.5));
+		c.PrintVector();
+		//cout << c << endl;
+	}
+	catch (const char* exp)
+	{
+		cout << exp << endl;
+	}
 	return 0;
 }
