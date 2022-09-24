4bd244cb73b24901c3b2bef98d42163b85fec33e
ZamotohinaMaria <zamotokhina.masha@mail.ru>
2022-09-24 18:39:03 +0400

    добавлены определения методов класса, перегрузки некоторых операторов и написан примерный код основной прораммы

--- Vector.h
+++ Vector.h
@@ -5,15 +5,22 @@ class Vectors
 private:
 	unsigned size;
 	double* vector;
+	static double MIN_D;
 public:
 	Vectors(unsigned size = 0);
 	Vectors(const Vectors& v);
 	~Vectors();
+	unsigned GetSize() const;
+	void SetVector();
+	void PrintVector() const;
 	Vectors& operator = (const Vectors& v);
-	double& operator [] (const unsigned& i);
-	Vectors& operator + (const Vectors& v);
-	Vectors& operator - (const Vectors& v);
-	double& operator * (const Vectors& v);
-	Vectors& operator * (const int& c); //обспечить коммутативность
-	Vectors& operator / (const int& c);
+	double& operator [] (const unsigned& i) const;
+	Vectors operator + (const Vectors& v);
+	Vectors operator - (const Vectors& v);
+	double operator * (const Vectors& v);
+	Vectors operator * (const int& c); //обспечить коммутативность
+	Vectors operator / (const int& c);
+	bool operator == (const Vectors& v) const;
+	bool operator != (const Vectors& v) const;
+	friend std::ostream& operator << (std::ostream& out, const Vectors& point);
 };
